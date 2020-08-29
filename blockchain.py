4aaafdfcf59eb05c89382df3693f3986b63e3e3f
jorgepiao <piaosin11@gmail.com>
2020-08-29 01:40:39 -0500

    Creadcion de EndPoints /chain, /mine y /pending_tx

--- blockchain.py
+++ blockchain.py
@@ -90,18 +90,43 @@ class Blockchain:
 app = Flask(__name__)
 blockchain = Blockchain()
 
-@app.route('/new_transaction', methods['POST'])
-    def new_transaction():
-        tx_data = request.get_json()
-        require_fields = ["author", "content"]
+@app.route('/new_transaction', methods=['POST'])
+def new_transaction():
+    tx_data = request.get_json()
+    require_fields = ["author", "content"]
 
-        for field in require_fields:
-            if not tx_data.get(field):
-                return 'Datos de transaccion invalidos', 404
+    for field in require_fields:
+        if not tx_data.get(field):
+            return 'Datos de transaccion invalidos', 404
 
-        tx_data["timestamp"] = time.time()
-        blockchain.new_transaction(tx_data)
-        return 'Exito', 201
+    tx_data["timestamp"] = time.time()
+    blockchain.new_transaction(tx_data)
+    return 'Exito', 201
+
+
+@app.route('/chain', methods=['GET'])
+def get_chain():
+    chain_data = []
+
+    for block in blockchain.chain:
+        chain_data.append(block.__dict__)
+        return json.dumps({"length": len(chain_data),
+                            "chain": chain_data})
+
+
+@app.route('/mine', methods=['GET'])
+def mine_unconfirmed_transaction():
+    result = blockchain.mine()
+
+    if not result:
+        return 'Nothing to mine'
+    return f'Block#{result} is mined'
+
+
+@app.route('/pending_tx', methods=['POST'])
+def get_pending_tx():
+    return json.dumps(blockchain.unconfirmed_transactions)
+
 
 
 
