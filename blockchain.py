63e6925ce98cdcf863602bf44ea4e29daceef420
jorgepiao <piaosin11@gmail.com>
2020-08-28 21:58:16 -0500

    Creacion de la clase Block

---
+++ blockchain.py
@@ -0,0 +1,18 @@
+
+import json
+from hashlib import sha256
+
+
+class Block:
+
+    def __init__(self, index, transactions, timestamp):
+        self.index = index
+        self.transactions = transactions
+        self.timestamp = timestamp
+
+    def compute_hash(block):
+        block_string = json.dump(self.__dict__, sort=True)
+        return sha256(block_string.encode()).hexdigest()
+
+    
+
