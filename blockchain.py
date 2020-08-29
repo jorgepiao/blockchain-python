6ce9ca5d7b344be76f4c9683955b1da7858a1527
jorgepiao <piaosin11@gmail.com>
2020-08-28 22:12:40 -0500

    Creacion de la clase Blockchain

--- blockchain.py
+++ blockchain.py
@@ -1,18 +1,37 @@
 
 import json
+import time
 from hashlib import sha256
 
 
 class Block:
 
-    def __init__(self, index, transactions, timestamp):
+    def __init__(self, index, transactions, timestamp, previous_hash):
         self.index = index
         self.transactions = transactions
         self.timestamp = timestamp
+        self.previous_hash = previous_hash
 
-    def compute_hash(block):
-        block_string = json.dump(self.__dict__, sort=True)
+    def compute_hash(self, block):
+        block_string = json.dumps(self.__dict__, sort_keys=True)
         return sha256(block_string.encode()).hexdigest()
+
+
+class Blockchain:
+
+    def __init__(self):
+        self.unconfirmed_transactions = []
+        self.chain = []
+        self.create_genesis_block()
+
+    def create_genesis_block(self):
+        genesis_block = Block(0, [], time.time(), '0'*64)
+        genesis_block.hash = genesis_block.compute_hash()
+        self.chain.append(genesis_block)
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
 
     
 
